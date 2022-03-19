5076c9e4e34e53d960b6c7b0d9d2bf106c6538ec
Jakub Rutrle <rutrle@seznam.cz>
2022-03-19 12:58:43 +0100

    blueprints succesfully created, need a bit of cleaning up

--- smart_ticket/__init__.py
+++ smart_ticket/__init__.py
@@ -15,7 +15,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
 from smart_ticket.user_bp.routes import user_bp
+from smart_ticket.ticket_bp.routes import ticket_bp
+
 app.register_blueprint(user_bp, url_prefix = '/user')
+app.register_blueprint(ticket_bp, url_prefix = '/ticket')
 
 from smart_ticket import routes, filters
 
