56a25d9d41139bfd4ef9846487a8e51bbeb8539a
Jakub Rutrle <jakubrutrle@gmail.com>
2022-04-22 01:15:51 +0200

    deactivating user functional

--- smart_ticket/admin_bp/routes.py
+++ smart_ticket/admin_bp/routes.py
@@ -6,15 +6,19 @@ from smart_ticket.admin_bp.forms import ConfirmUserDeactivationForm
 
 admin_bp = Blueprint('admin_bp', __name__, template_folder='templates')
 
-def deactivate_user(user_id:int):
+def deactivate_user(user):
+    user.is_active = False
+    user.currently_solving.clear()
+
+    db.session.add(user)
+    db.session.commit()
+
+def reactivate_user(user):
     pass
 
-def reactivate_user(user_id:int):
-    pass
 
 
-
-@admin_bp.route('')
+@admin_bp.route('', methods=['GET', 'POST'])
 @login_required
 @admin_required
 def admin_page():
@@ -22,8 +26,10 @@ def admin_page():
     inactive_users = db.session.query(User).filter(User.is_active == False).order_by('creation_time')
     user_deactivation_form = ConfirmUserDeactivationForm()
 
-    if user_deactivation_form.validate_on_submit():
-        User.query.filter_by(username=user_deactivation_form.username.data).first()
+    if user_deactivation_form.validate_on_submit() and  current_user.check_attempted_password(user_deactivation_form.password.data):
+        user_to_deactivate = User.query.filter_by(id=user_deactivation_form.user_id.data).first()
+        if user_to_deactivate.username == user_deactivation_form.user_username.data:
+            deactivate_user(user_to_deactivate)
 
     return render_template("admin_bp/admin_tools.html", active_users=active_users, inactive_users=inactive_users, user_deactivation_form = user_deactivation_form)
    
