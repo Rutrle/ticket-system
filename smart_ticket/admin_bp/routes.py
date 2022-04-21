01fdd50a0a71f62e17310aab73330c81d62356c7
Jakub Rutrle <jakubrutrle@gmail.com>
2022-04-22 01:37:26 +0200

    added flashes for deactivating a user

--- smart_ticket/admin_bp/routes.py
+++ smart_ticket/admin_bp/routes.py
@@ -14,8 +14,9 @@ def deactivate_user(user):
     db.session.commit()
 
 def reactivate_user(user):
-    pass
-
+    user.is_active = True
+    db.session.add(user)
+    db.session.commit()
 
 
 @admin_bp.route('', methods=['GET', 'POST'])
@@ -26,10 +27,20 @@ def admin_page():
     inactive_users = db.session.query(User).filter(User.is_active == False).order_by('creation_time')
     user_deactivation_form = ConfirmUserDeactivationForm()
 
-    if user_deactivation_form.validate_on_submit() and  current_user.check_attempted_password(user_deactivation_form.password.data):
+    if user_deactivation_form.validate_on_submit():
         user_to_deactivate = User.query.filter_by(id=user_deactivation_form.user_id.data).first()
-        if user_to_deactivate.username == user_deactivation_form.user_username.data:
+        password_correct = current_user.check_attempted_password(user_deactivation_form.password.data)
+        user_username_correct = user_to_deactivate.username == user_deactivation_form.user_username.data
+        if password_correct and user_username_correct:
             deactivate_user(user_to_deactivate)
+            flash(f"Account of user {user_to_deactivate.username} was succesfully deactivated", category="success")
 
+        else:
+            flash(f'There was an error with deactivating a user {user_to_deactivate.username}, please check if you entered valid username and password', category='danger')
+
+    elif user_deactivation_form.errors != {}:
+        for err_msg in user_deactivation_form.errors.values():
+            flash(f'There was an error with deactivating a user: {err_msg[0]}', category='danger')
+            
     return render_template("admin_bp/admin_tools.html", active_users=active_users, inactive_users=inactive_users, user_deactivation_form = user_deactivation_form)
    
