5023914847fcde71d924e83d9a78d48d4044d9d5
Jakub Rutrle <jakubrutrle@gmail.com>
2022-05-07 19:25:51 +0200

    filters refactoring

--- smart_ticket/filters.py
+++ smart_ticket/filters.py
@@ -1,6 +1,10 @@
 from smart_ticket import app
+from datetime import datetime
 
 
 @app.template_filter('format_time')
-def format_time(timestamp):
+def format_time(timestamp: datetime) -> str:
+    """
+    returns readable string from datetime object 'timestamp'
+    """
     return timestamp.strftime("%d/%m/%Y, %H:%M")
