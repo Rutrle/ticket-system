7030df21cf4d7b7ad95ffcca89dfb133d1fc774b
Jakub Rutrle <jakubrutrle@gmail.com>
2022-05-07 20:14:05 +0200

    refactoring of general routes

--- smart_ticket/routes.py
+++ smart_ticket/routes.py
@@ -1,13 +1,18 @@
-from flask_login import login_required
 from smart_ticket import app
-from flask import render_template
+from flask import render_template, Response
 
 
 @app.route('/')
-def home_page():
+def home_page() -> Response:
+    """
+    simple home page
+    """
     return render_template('index.html')
 
 
 @app.route('/about')
-def about_page():
+def about_page() -> Response:
+    """
+    page with information about SmartTicket
+    """
     return render_template('about.html')
