31a3faab774bf1707579711f1c7a36a23634be44
Jakub Rutrle <jakubrutrle@gmail.com>
2022-05-08 13:45:00 +0200

    solver added properly to solved tickets

--- db_setup.py
+++ db_setup.py
@@ -28,22 +28,25 @@ def database_reset() -> None:
         db.session.add(user_role)
     db.session.commit()
 
-    admin = User(username='admin', email='admin@mail.com', password='admin', user_role=db.session.query(UserRole).filter_by(name="admin").first())
+    admin = User(username='admin', email='admin@mail.com', password='admin',
+                 user_role=db.session.query(UserRole).filter_by(name="admin").first())
 
     db.session.add(admin)
     db.session.commit()
 
-def create_dummy_tickets(ticket_num:int = 50)->None:
+
+def create_dummy_tickets(ticket_num: int = 50) -> None:
     """
     fill the database with 'ticket_num' amount od random ticket (dummy data)
     only for testing/showing
     """
     fake = Faker()
-    tickets =[]
+    tickets = []
     max_user_id = db.session.query(func.max(User.id)).scalar()
 
     for i in range(ticket_num):
-        tickets.append(Ticket(subject=fake.unique.sentence(), issue_description=fake.text(), creation_time=(datetime.now() - timedelta(days=randint(0, 180))), author_id = randint(1,max_user_id)))
+        tickets.append(Ticket(subject=fake.unique.sentence(), issue_description=fake.text(), creation_time=(
+            datetime.now() - timedelta(days=randint(0, 180))), author_id=randint(1, max_user_id)))
 
     for ticket in tickets:
         db.session.add(ticket)
@@ -52,50 +55,58 @@ def create_dummy_tickets(ticket_num:int = 50)->None:
     ticket_creation_logs = []
 
     for ticket in tickets:
-        ticket_creation_logs.append(TicketLogMessage(ticket_id=ticket.id, message_text="Ticket opened", message_category="sys_message"))
+        ticket_creation_logs.append(TicketLogMessage(
+            ticket_id=ticket.id, message_text="Ticket opened", message_category="sys_message"))
 
     for ticket_log in ticket_creation_logs:
         db.session.add(ticket_log)
 
     db.session.commit()
 
-def create_solved_dummy_tickets(ticket_num:int = 50)->None:
+
+def create_solved_dummy_tickets(ticket_num: int = 50) -> None:
     """
     fill the database with 'ticket_num' amount od random solved ticket (dummy data)
     only for testing/showing
     """
     fake = Faker()
-    tickets =[]
+    tickets = []
     max_user_id = db.session.query(func.max(User.id)).scalar()
 
     for i in range(ticket_num):
-        tickets.append(Ticket(subject=fake.unique.sentence(), issue_description=fake.text(), creation_time=(datetime.now() - timedelta(days=randint(180, 360))), author_id = randint(1,max_user_id), solved_on=(datetime.now() - timedelta(days=randint(0, 180))), solver_id=randint(1,max_user_id), is_solved=True))
+        tickets.append(Ticket(subject=fake.unique.sentence(), issue_description=fake.text(), creation_time=(datetime.now() - timedelta(days=randint(180, 360))),
+                       author_id=randint(1, max_user_id), solved_on=(datetime.now() - timedelta(days=randint(0, 180))), solver_id=randint(1, max_user_id), is_solved=True))
 
     for ticket in tickets:
         db.session.add(ticket)
     db.session.commit()
-    
+
     ticket_creation_logs = []
 
     for ticket in tickets:
-        ticket_creation_logs.append(TicketLogMessage(ticket_id=ticket.id, message_text="Ticket opened", message_category="sys_message"))
-        ticket_creation_logs.append(TicketLogMessage(ticket_id=ticket.id, message_text=fake.text(), message_category="solved"))
+        ticket_creation_logs.append(TicketLogMessage(
+            ticket_id=ticket.id, message_text="Ticket opened", message_category="sys_message"))
+        ticket_creation_logs.append(TicketLogMessage(ticket_id=ticket.id, message_text=fake.text(
+        ), message_category="solved", author_id=ticket.solver_id))
 
     for ticket_log in ticket_creation_logs:
         db.session.add(ticket_log)
 
     db.session.commit()
 
-def create_dummy_users(users_num:int = 50, locale:str = "DE")->None:
+
+def create_dummy_users(users_num: int = 50, locale: str = "DE") -> None:
     """
     fills in database with 'user_num' amount of random dummy user profiles from 'locale' localization
+    only for testing/showing
     """
-    fake = Faker(locale=locale) # locale is used to prevent phone number extensions to be generated in phone numbers
-    users =[]
+    fake = Faker(
+        locale=locale)  # locale is used to prevent phone number extensions to be generated in phone numbers
+    users = []
 
     for i in range(users_num):
         users.append(User(username=fake.unique.name(), email=fake.unique.free_email(), password=fake.password(),
-                    phone_number=fake.phone_number()  , user_role=db.session.query(UserRole).filter_by(name="user").first()))
+                          phone_number=fake.phone_number(), user_role=db.session.query(UserRole).filter_by(name="user").first()))
 
     for user in users:
         db.session.add(user)
@@ -105,6 +116,7 @@ def create_dummy_users(users_num:int = 50, locale:str = "DE")->None:
 def fill_in_database() -> None:
     """
     fill the database with defined dummy data so the functionality of SmartTicket can be seen/tested
+    only for testing/showing
     """
     long_text = "Est eiusmod sunt in velit cillum enim consectetur pariatur ullamco. Quis pariatur anim deserunt irure voluptate aute reprehenderit enim minim aliquip laboris et. Nostrud consectetur ex non ullamco Lorem commodo. Cillum magna dolore occaecat duis laborum consequat. Duis Lorem reprehenderit ex dolore elit excepteur in qui cupidatat in reprehenderit. Id eu minim elit mollit. Occaecat ea ipsum do ullamco laborum nostrud ipsum Lorem ullamco elit."
     longer_text = """Ullamco deserunt deserunt officia ad nisi consequat adipisicing velit cupidatat nulla qui enim magna. Esse sint esse proident enim elit dolor amet. Velit culpa amet enim dolore est.
@@ -147,11 +159,16 @@ def fill_in_database() -> None:
     u1 = User.query.filter_by(username='User1').first()
     u2 = User.query.filter_by(username='Greorge Sharp').first()
 
-    tickets.append(Ticket(subject='Something broke', issue_description="something has broken, it's not my fault"))
-    tickets.append(Ticket(subject='Something else broke',issue_description=long_text))
-    tickets.append(Ticket(subject='Lorem ipsum',issue_description=longer_text))
-    tickets.append(Ticket(subject='Owned ticket',issue_description="I was created by user3", author_id=u.id))
-    tickets.append(Ticket(subject='Coffee is cold', issue_description="Coffee machine on floor 3 makes coffee too cold", author_id=u1.id))
+    tickets.append(Ticket(subject='Something broke',
+                   issue_description="something has broken, it's not my fault"))
+    tickets.append(Ticket(subject='Something else broke',
+                   issue_description=long_text))
+    tickets.append(Ticket(subject='Lorem ipsum',
+                   issue_description=longer_text))
+    tickets.append(Ticket(subject='Owned ticket',
+                   issue_description="I was created by user3", author_id=u.id))
+    tickets.append(Ticket(subject='Coffee is cold',
+                   issue_description="Coffee machine on floor 3 makes coffee too cold", author_id=u1.id))
     tickets.append(Ticket(subject='Solved ticket', issue_description="I was solved by user3",
                    solved_on=datetime.now(), author_id=u.id, solver_id=u.id, is_solved=True))
     tickets.append(Ticket(subject='Keyboard not working', issue_description="Keyboard is not working",
@@ -160,10 +177,14 @@ def fill_in_database() -> None:
                    solved_on=datetime.now() - timedelta(days=5), solver_id=u2.id, is_solved=True))
     tickets.append(Ticket(subject='Browser not functioning properly', issue_description="I was solved by user2",
                    solved_on=datetime.now() - timedelta(days=5), solver_id=u1.id, is_solved=True))
-    tickets.append(Ticket(subject='Room is too dark',issue_description="Room that I'm sitting in has too many broken lightbulbs so it's dark in it"))
-    tickets.append(Ticket(subject='Room is too brigh', issue_description="Room that I'm sitting in has too many lightbulbs so it's too bright in it"))
-    tickets.append(Ticket(subject='Too many bugs', issue_description="There are too many bugs in program XYZ"))
-    tickets.append(Ticket(subject='Coffee is hot', issue_description="Coffee machine on floor 2 makes coffee too hot", author_id=u1.id))
+    tickets.append(Ticket(subject='Room is too dark',
+                   issue_description="Room that I'm sitting in has too many broken lightbulbs so it's dark in it"))
+    tickets.append(Ticket(subject='Room is too brigh',
+                   issue_description="Room that I'm sitting in has too many lightbulbs so it's too bright in it"))
+    tickets.append(Ticket(subject='Too many bugs',
+                   issue_description="There are too many bugs in program XYZ"))
+    tickets.append(Ticket(subject='Coffee is hot',
+                   issue_description="Coffee machine on floor 2 makes coffee too hot", author_id=u1.id))
 
     for ticket in tickets:
         db.session.add(ticket)
@@ -183,8 +204,8 @@ def fill_in_database() -> None:
 if __name__ == "__main__":
     database_reset()
     fill_in_database()
-    create_dummy_tickets()
     create_solved_dummy_tickets()
+    create_dummy_tickets()
     create_dummy_users(10, "CZ")
     create_dummy_users(10, "DE")
     create_dummy_users(10, "en_GB")
